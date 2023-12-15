3ec8c4f826216a1521a1714a3acced47a4c11e6d
Mac <mac@Macs-MacBook-Pro.local>
2023-12-15 18:07:29 +0530

    fix for csv convertor

--- index.js
+++ index.js
@@ -1,99 +1,86 @@
-const fs = require('fs');
-const XLSX = require('xlsx');
-const { v4: uuidv4 } = require('uuid');
+const fs = require("fs");
+const XLSX = require("xlsx");
+
+function generateUUID() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
 
 function convertExcelToJSON(filePath) {
   const workbook = XLSX.readFile(filePath);
   const worksheet = workbook.Sheets[workbook.SheetNames[0]];
   const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
 
-  const headers = jsonData[0];
-  const jsonResult = {
-    data : {},
-    properties : {}
-  };
-  let Route;
+  const result = {};
 
-  for (let i = 1; i < jsonData.length; i++) {
-    const values = jsonData[i];
-    const row = {};
+  let currentPath = null;
 
-    for (let j = 0; j < headers.length; j++) {
-      const header = headers[j];
-      const value = values[j];
+  jsonData.forEach((entry) => {
+    if (entry.length === 2) {
+      currentPath = entry[1];
+      result[currentPath] = result[currentPath] || [];
+    } else if (entry.length > 2) {
+      const category = {
+        id: generateUUID(),
+        image: entry[3],
+        name: entry[2],
+        childstories: [],
+      };
 
-      if (header && value) {
-        row[header] = value;
+      if (result[currentPath]) {
+        result[currentPath].push(category);
       }
-    }
 
-    if (row?.pathname) {
-      console.log("sahi");
-      Route = row?.pathname;
-      if (!jsonResult.data[Route]) {
-        jsonResult.data[Route] = []; // Initialize as an empty array if it doesn't exist
-      }
-    }
+      const addDots = (substory, productNames) => {
+        productNames.forEach((productName) => {
+          substory.dots.push({
+            id: generateUUID(),
+            productname: productName.trim(),
+          });
+        });
+      };
 
-    const Thumbnail = row?.['Thumbnail'];
-    const storyname = row?.['storyname'];
-    const collection = {
-      id: uuidv4(), // Use uuidv4() to generate a unique ID
-      image: Thumbnail,
-      name: storyname,
-      childstories: [],
-    };
-
-    for (let k = 1; k <= 5; k++) {
-      if (Thumbnail) {
-        const StoryIDKey = `substory${k}`;
-        const productIDKey = `product${k}`;
-        const storiescontnet = row?.[StoryIDKey];
-        const productIDValue = row?.[productIDKey];
-
-        // Check if productIDValue is defined
-        if (productIDValue !== undefined) {
-          // Check if productIDValue contains a comma
-          if (productIDValue.includes(',')) {
-            const productNames = productIDValue.split(',');
-
-            // Loop through product names and add them to the collection
-            productNames.forEach((productName) => {
-              collection.childstories.push({
-                id: uuidv4(), // Use uuidv4() to generate a unique ID
-                storiescontnet: storiescontnet,
-                dots: [{ id: uuidv4(), productname: productName.trim() }],
-              });
-            });
-          } else {
-            // If there's no comma, treat it as a single product
-            collection.childstories.push({
-              id: uuidv4(), // Use uuidv4() to generate a unique ID
-              storiescontnet: storiescontnet,
-              dots: [{ id: uuidv4(), productname: productIDValue }],
-            });
-          }
+      for (let i = 4; i < entry.length; i += 2) {
+        if (entry[i]) {
+          const substory = {
+            id: generateUUID(),
+            storiescontnet: entry[i],
+            dots: [],
+          };
+          addDots(substory, entry[i + 1].split(","));
+          category.childstories.push(substory);
         }
       }
     }
-
-    if (Thumbnail) {
-      console.log(Thumbnail, "Thumbnail");
-      jsonResult.data[Route].push(collection);
-    }
-  } 
-
-  jsonResult.properties = {
-    bg : "#FF0100"
-  }
-  
-  fs.writeFile('shopclips.json', JSON.stringify(jsonResult, null, 2), function (err) {
-    if (err) throw err;
-    console.log('Saved as JSON file');
   });
 
-  return jsonResult;
+  const json = {
+    data: result,
+    properties: {
+      bg: "#ffff"
+    }
+  }
+
+
+  // Please change the shopName as per the client
+  const shopName = 'ekkatha'
+  const fileName = `${shopName}.json`
+
+  fs.writeFile(
+    fileName,
+    JSON.stringify(json, null, 2),
+    function (err) {
+      if (err) throw err;
+      console.log(`Saved as ${fileName} file`);
+    }
+  );
+
+  return json;
 }
 
-const filePath = './Memara shopclips data - Sheet1.csv';
-const jsonResult = convertExcelToJSON(filePath);
+const filePath = "./kadamhaat.csv";
+convertExcelToJSON(filePath);
+
